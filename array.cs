f680abed0f963470c8a62c1b0da7e18148431f45
ubaiubai <93110118+ubaiubai@users.noreply.github.com>
2021-10-26 20:56:45 -0700

    Add files via upload

---
+++ array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number of employees: ");
+            int employees = int.Parse(Console.ReadLine());
+            int[] salaryArray = new int[employees];
+   
+            for (int i = 0; i < employees; i++)
+            {
+                Console.WriteLine("{0}. salary of employee: ", i + 1);
+                salaryArray[i] = int.Parse(Console.ReadLine());
+            }
+            Console.ReadKey();
+            Console.ReadLine();
+        }
+    }
+}
