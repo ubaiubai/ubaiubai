f2bcc4b618be5c92a817b50578b06c07fe22a217
ubaiubai <93110118+ubaiubai@users.noreply.github.com>
2021-10-27 22:00:23 -0700

    Add files via upload

---
+++ armstrong.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Armstrong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, rem, temp, sum = 0;
+            Console.WriteLine("Check Number Is Armstrong Or Not");
+            Console.Write("Enter Your Number To Check :: ");
+            number = int.Parse(Console.ReadLine());
+
+            temp = number;
+            while (number > 0)
+            {
+                rem = number % 10;
+                sum = sum + (rem * rem * rem);
+                number = number / 10;
+            }
+
+            if (temp == sum)
+                Console.WriteLine(temp + " Is A Armstrong Number");
+            else
+                Console.WriteLine(temp + " Is Not A Armstrong Number");
+            Console.ReadKey();
+        }
+    }
+}
