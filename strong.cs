026f77c0019dbac322f4440589441cd0a796b671
ubaiubai <93110118+ubaiubai@users.noreply.github.com>
2021-10-27 22:00:01 -0700

    Add files via upload

---
+++ strong.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class strong
+{
+    static int getFactorial(int number)
+    {
+        int factorial = 1;
+        for (int i = number; i > 1; i--)
+        {
+            factorial = factorial * i;
+        }
+        return factorial;
+    }
+
+    public static void Main()
+    {
+        int givenNumber;
+
+        Console.WriteLine("Enter a number to check");
+        givenNumber = Convert.ToInt32(Console.ReadLine());
+
+        int sum = 0;
+        int copyNumber = givenNumber;
+
+        while (copyNumber != 0)
+        {
+            int lastDigit = copyNumber % 10;
+            sum = sum + getFactorial(lastDigit);
+            copyNumber = copyNumber / 10;
+        }
+
+        if (sum == givenNumber)
+        {
+            Console.WriteLine("Given number is a strong number");
+        }
+        else
+        {
+            Console.WriteLine("Given number is not a strong number");
+
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+
+    }
+}
