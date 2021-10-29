0ba6ae16b68c0ef8eb7fe1c1707540c866d02361
ubaiubai <93110118+ubaiubai@users.noreply.github.com>
2021-10-28 21:35:32 -0700

    Add files via upload

---
+++ string.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder details;
+            details = new StringBuilder();
+            Console.WriteLine("enter your name");
+            string username = Console.ReadLine();
+            
+
+            Console.WriteLine("enter your age");
+             int age =int.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine("enter your experience");
+            int  experience = int.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine("enter your qulification");
+            string qualification = Console.ReadLine();
+            
+
+            Console.WriteLine("name {0} , age{1}  , exp{3} , qualification{4} ", username, age,  experience, qualification);
+            Console.ReadLine();
+
+
+        }
+    }
+}
