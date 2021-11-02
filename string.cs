837e520b11d9aea0d5d4ac369e0e63efffc49a20
ubaiubai <93110118+ubaiubai@users.noreply.github.com>
2021-11-01 20:58:16 -0700

    Add files via upload

--- string.cs
+++ string.cs
@@ -1,35 +1,39 @@
 ﻿using System;
+
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace string_1
+namespace string2
 {
     class Program
     {
         static void Main(string[] args)
         {
+
             StringBuilder details;
             details = new StringBuilder();
-            Console.WriteLine("enter your name");
-            string username = Console.ReadLine();
-            
+            Console.WriteLine("Enter name");
+            string name = Console.ReadLine();
 
-            Console.WriteLine("enter your age");
-             int age =int.Parse(Console.ReadLine());
-            
+            Console.WriteLine("Enter age");
+            string age = Console.ReadLine();
 
-            Console.WriteLine("enter your experience");
-            int  experience = int.Parse(Console.ReadLine());
-            
+            Console.WriteLine("Enter address");
+            string address = Console.ReadLine();
 
-            Console.WriteLine("enter your qulification");
+            Console.WriteLine("Enter contact number");
+            string contactnumber = Console.ReadLine();
+
+            Console.WriteLine("Enter qualification");
             string qualification = Console.ReadLine();
-            
 
-            Console.WriteLine("name {0} , age{1}  , exp{3} , qualification{4} ", username, age,  experience, qualification);
-            Console.ReadLine();
+            Console.WriteLine("Enter experience");
+            string experience = Console.ReadLine();
 
+            Console.WriteLine("name {0} , age {1} , address {2} , contact number {3} , qualification {4} and experience {5} ", name, age, address, contactnumber, qualification, experience);
 
         }
     }
