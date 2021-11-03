1441431649540cc337f187dccdc88653c1093de3
ubaiubai <93110118+ubaiubai@users.noreply.github.com>
2021-11-02 22:19:37 -0700

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class1
+
+{
+    class company
+    {
+        
+        string name;
+        string sector;
+        string location;
+        int age;
+
+
+        
+        public string Name { get => name; set => name = value; }
+        public string Sector { get => sector; set => sector = value; }
+        public string Location { get => location; set => location = value; }
+        public int Age { get => age; set => age = value; }
+
+        //Parameterized constructor
+        public company(string name, string sector, string location, int age)
+        {
+            this.name = name;
+            this.sector = sector;
+            this.location = location;
+            this.age = age;
+        }
+        company()
+        {
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            company com = (company)obj;
+            return this.name.Equals(com.name) && this.location.Equals(com.location);
+        }
+
+
+
+
+        
+        public override string ToString()
+        {
+            return String.Format("name is {0} \n sector is {1} \n location is : {2} \n age is : {3}", name, sector, location, age);
+        }
+
+        
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static company GetCompanyDetails(String comaccdetails)
+        {
+            string[] c = comaccdetails.Split(',');
+            company com_details = new company(c[0], c[1], c[2], int.Parse(c[3]));
+            return com_details;
+        }
+
+
+    }
+}
---
+++ company.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace company
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of companies");
+            int com_count;
+            com_count = Convert.ToInt32(Console.ReadLine());
+            company[] com_obj = new company[com_count];
+
+            int count = 0;
+            for (int i = 0; i < com_count; i++)
+            {
+                Console.WriteLine("Enter company {0} details seperated by , : name,sector,location,age ", i + 1);
+                string comdetails = Console.ReadLine();
+                company c = company.GetCompanyDetails(comdetails);
+
+                if (!com_obj.Contains(c))
+                {
+                    com_obj[i] = c;
+                    count++;
+
+                }
+                else if (com_obj.Contains(c))
+                {
+                    Console.WriteLine("companies1 is same as company2");
+
+
+                }
+                if (count == 2)
+                {
+                    Console.WriteLine("Bothe companies are different");
+                }
+
+
+
+            }
+
+            foreach (var item in com_obj)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
