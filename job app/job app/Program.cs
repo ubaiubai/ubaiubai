27bf131d0cd32fb0906375b7f97384b6358a3492
ubaiubai <93110118+ubaiubai@users.noreply.github.com>
2021-11-18 21:56:19 -0800

    Add files via upload

---
+++ job app/job app/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JOB DETAILS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Job[] j1;
+            int count = int.Parse(Console.ReadLine());
+            j1 = new Job[count];
+            for (int i = 0; i < count; i++)
+            {
+                string role;
+                string skillset;
+                string emptype;
+                string location;
+                int exp;
+                double sal;
+
+                Console.WriteLine("Enter the Role:");
+                role = Console.ReadLine();
+                Console.WriteLine("Enter the Skillset:");
+                skillset = Console.ReadLine();
+                Console.WriteLine("Enter the Emptype");
+                emptype = Console.ReadLine();
+                Console.WriteLine("Enter the Location:");
+                location = Console.ReadLine();
+                Console.WriteLine("Enter the Minimum Experience");
+                exp = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the Salary");
+                sal = Convert.ToInt64(Console.ReadLine());
+                Job j2 = new Job(role, skillset, emptype, location, exp, sal);
+
+            }
+
+            SortbySalary sal1 = new SortbySalary();
+            Array.Sort(j1, sal1);
+            foreach (var item in j1)
+
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.Read();
+        }
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ job app/job.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JOB DETAILS
+{
+    class Job : IComparable
+    {
+        string role;
+        string skillset;
+        string emptype;
+        string location;
+        int exp;
+        double sal;
+
+        public Job()
+        {
+        }
+
+        public Job(string role, string skillset, string emptype, string location, int exp, double sal)
+        {
+            Role = role;
+            Skillset = skillset;
+            Emptype = emptype;
+            Location = location;
+            Exp = exp;
+            Sal = sal;
+        }
+
+        public string Role { get => role; set => role = value; }
+        public string Skillset { get => skillset; set => skillset = value; }
+        public string Emptype { get => emptype; set => emptype = value; }
+        public string Location { get => location; set => location = value; }
+        public int Exp { get => exp; set => exp = value; }
+        public double Sal { get => sal; set => sal = value; }
+
+        public int CompareTo(object obj)
+        {
+            Job j1 = (Job)obj;
+            return this.Exp.CompareTo(j1.Exp);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Role : {0}\nSkillset : {1}\nEmptype : {2}\nLocation: {3}\nExp: {4}\nSal: {5}", Role, Skillset, Emptype, Location);
+        }
+    }
+}
